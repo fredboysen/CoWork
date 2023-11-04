4401ce02244825fd07f743985e68fdac848aa18c
Frederik Boysen <freboysen@gmail.com>
2023-11-04 02:15:05 +0100

    update

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const { readFile } = require("fs").promises;
-const express = require("express");
+const { readFile } = require('fs').promises;
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.get("/", async (request, response) => {
-  response.send(await readFile("./index.html", "utf8"));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (request, response) => {
+    response.send(await readFile('./index.html', 'utf8'));
+ 
 });
 
+
 app.listen(process.env.PORT || 3000, () => {
-  console.log("App available on http://localhost:3000");
+  console.log('App available on http://localhost:3000');
 });
+
