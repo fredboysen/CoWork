67be5145028e601273cc101d3bd654a9619231d0
Frederik Boysen <freboysen@gmail.com>
2023-11-04 02:30:35 +0100

    adding static elements and views

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+//imports
 const { readFile } = require("fs").promises;
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
+app.use("/styles", express.static(__dirname + "public/styles"));
+app.use("/scripts", express.static(__dirname + "public/scripts"));
+app.use("/images", express.static(__dirname + "public/images"));
 
 app.get("/", async (request, response) => {
   response.send(await readFile("views/index.html", "utf8"));
 });
 
+//listen on port 3000
 app.listen(process.env.PORT || 3000, () => {
   console.log("App available on http://localhost:3000");
 });
