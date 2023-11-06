991f8d6a87e9ee5afa42a0d83a669a305a37ce6e
Frederik Boysen <freboysen@gmail.com>
2023-11-06 01:25:57 +0100

    Revert "moving folders"
    This reverts commit 9194ac8a5b016f8419fcd2555d5fd0e332e6c495.

---
+++ README.md
@@ -0,0 +1,5 @@
+# CoWork
+
+A simple job portal web application catered for students. A platform where students can grow competences and business has the ability to quickly scout for new talent.
+
+
---
+++ app.js
@@ -0,0 +1,24 @@
+//imports
+const { readFile } = require("fs").promises;
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use("/styles", express.static(__dirname + "public/styles"));
+app.use("/scripts", express.static(__dirname + "public/scripts"));
+app.use("/images", express.static(__dirname + "public/images"));
+
+//setting views
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("", (req, res) => {
+  res.render("index");
+});
+
+//listen on port 3000
+app.listen(process.env.PORT || 3000, () => {
+  console.log("App available on http://localhost:3000");
+});
