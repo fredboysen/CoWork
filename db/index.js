a40c4a649a1bf05b5e911abb3b0df2a3166ccdca
fredwerb <frebo@live.dk>
2023-11-27 14:23:44 +0100

    update

--- db/index.js
+++ db/index.js
@@ -1,1 +1,31 @@
 //database connection code:
+const path = require("path");
+require("dotenv").config({
+  override: true,
+  path: path.join(__dirname, "development.env"),
+});
+const { Pool, Client} = require("pg");
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env_DATABASE,
+    password: process.env_PASSWORD,
+    port: process.env.DB_PORT || 5432, // Default PostgreSQL port
+  });
+  
+  // Listen for the 'connect' event
+  pool.on('connect', () => {
+    console.log('Connected to the database');
+  });
+pool.query("SELECT NOW()", (err, result) => {
+  if (err) {
+    console.error("Error executing query", err);
+  } else {
+    console.log("Current date and time from the database:", result.rows[0].now);
+  }
+
+  // Close the pool (optional, as the pool will be closed when the application exits)
+  pool.end();
+});
+
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-//imports
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static("public"));
-app.use("/styles", express.static(__dirname + "public/styles"));
-app.use("/scripts", express.static(__dirname + "public/scripts"));
-app.use("/images", express.static(__dirname + "public/images"));
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
-//setting views
-app.set("views", "./views");
-app.set("view engine", "ejs");
+// Set up routes for specific static subdirectories
+app.use("/styles", express.static(path.join(__dirname, "public/styles")));
+app.use("/scripts", express.static(path.join(__dirname, "public/scripts")));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
+// Render the "index.html" file
 app.get("", (req, res) => {
-  res.render("index");
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-//listen on port 3000
-app.listen(process.env.PORT || 3000, () => {
-  console.log("App available on http://localhost:3000");
+// Listen on port 3000 or the provided environment variable
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App available on http://localhost:${port}`);
 });
+
