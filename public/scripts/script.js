30e0865a97fcd579b629bed360c1bc0a06f947a7
Thomas Schelde <Thomas_Schelde@hotmail.com>
2023-12-01 23:52:25 +0100

    newloginpage

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -15,9 +15,6 @@ function toggleMenu() {
     }
   }
 
-
-
-
   function redirectToProfile() {
     // Perform any necessary login validation or actions here
 
@@ -26,8 +23,18 @@ function toggleMenu() {
     return false; // Prevent the default form submission
   }
 
-
-
-
-
+  document.addEventListener("DOMContentLoaded", function() {
+    // Your script here
+    const container = document.getElementById('container');
+    const registerBtn = document.getElementById('register');
+    const loginBtn = document.getElementById('login');
+  
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
+    });
+  
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
+});
 
