b03247cb4f57b65f42668e865f4fa6ddfba51b27
Thomas Schelde <Thomas_Schelde@hotmail.com>
2023-12-01 13:57:46 +0100

    missingsomething

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,7 @@
 $.get("xml/person.xml", function (xml, status) {
-    var firstname = $(xml).find("firstname");
-    var displayName = $(firstname[0]).text();
-    $("#textField1").html(displayName);
+  var firstname = $(xml).find("firstname");
+  var displayName = $(firstname[0]).text();
+  $("#textField1").html(displayName);
 });
 
 
