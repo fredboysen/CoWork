0ae1cb29629afbe17681a07b4e73c59f26a1513b
Frederik Boysen <freboysen@gmail.com>
2023-12-04 16:47:24 +0100

    up

--- db/index.js
+++ db/index.js
@@ -1,11 +1,14 @@
-const { Pool } = require("pg");
 require('dotenv').config();
+const { Pool } = require("pg");
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: process.env.DB_SSL === 'true', // You can use SSL if needed
+  // Add the following line to specify MD5 authentication
+  password_encryption: 'md5',
 });
 
 
